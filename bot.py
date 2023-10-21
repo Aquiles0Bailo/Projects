27462bcb10890811e98ffbc2ec756f09e7ac9a57
Aquiles0Bailo <147207311+Aquiles0Bailo@users.noreply.github.com>
2023-10-21 16:07:10 -0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.group()
+async def cool(ctx):
+    """Says if a user is cool.
+
+    In reality this just checks if a subcommand is being invoked.
+    """
+    if ctx.invoked_subcommand is None:
+        await ctx.send(f'No, {ctx.subcommand_passed} is not cool')
+
+bot.run("MTE2NTMyMDY4MzQyNTg5MDQwNQ.GMGflO.aijWOvTQoNnDU1Sps4k0exe_zSKqRzav9iwNXA")
+
